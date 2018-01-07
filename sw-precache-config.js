d844af3f2a4b41b1e35ab3abbe0e58bf6a49d1a4
alkaj <alkaj09@gmail.com>
2018-01-07 02:55:47 +0000

    adjusting and adding the challenge menu as well as disabling caching for src and images for now

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,9 +3,7 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-    '/src/**/*',
     '/scripts/**/*',
-    '/images/**/*',
     '/data/**/*'
   ],
   navigateFallback: '/index.html',
